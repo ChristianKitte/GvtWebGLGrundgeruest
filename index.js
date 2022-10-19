e887c6a2f890a2097e56957cd437b3fadfceccc6
Christian Kitte <chkitte@web.de>
2022-10-19 16:22:24 +0200

    Datenübergabe auf Uniforms eingefügt

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const gl = canvas.getContext("webgl2");
 
 const vertexShaderSource = `#version 300 es
     
+    uniform vec2 uPointPosition;
+    uniform float uPointSize;    
+    
     void main()
     {
-       gl_Position=vec4(0.5, 0.5, 0.0, 1.0);
-       gl_PointSize=150.0;
+        // uPointPosition wird übernommen, der Rest statisch aufgefüllt
+        gl_Position=vec4(uPointPosition, 0.0, 1.0);
+        gl_PointSize=uPointSize;
+        
+       //gl_Position=vec4(0.5, 0.5, 0.0, 1.0);
+       //gl_PointSize=150.0;
     }
 `;
 
@@ -21,25 +28,31 @@ const fragmentShaderSource = `#version 300 es
     }
 `;
 
-const programm = gl.createProgram();
+const program = gl.createProgram();
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, vertexShaderSource);
 gl.compileShader(vertexShader);
-gl.attachShader(programm, vertexShader);
+gl.attachShader(program, vertexShader);
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, fragmentShaderSource);
 gl.compileShader(fragmentShader);
-gl.attachShader(programm, fragmentShader);
+gl.attachShader(program, fragmentShader);
 
-gl.linkProgram(programm);
+gl.linkProgram(program);
 
-if (!gl.getProgramParameter(programm, gl.LINK_STATUS)) {
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
     console.log(gl.getShaderInfoLog(vertexShader));
     console.log(gl.getShaderInfoLog(fragmentShader));
 }
 
-gl.useProgram(programm);
+gl.useProgram(program);
+
+const uPointSizeLocation = gl.getUniformLocation(program, "uPointSize");
+const uPointPositionLocation = gl.getUniformLocation(program, "uPointPosition");
+
+gl.uniform2f(uPointPositionLocation, 0.8, 0.2);
+gl.uniform1f(uPointSizeLocation, 25.0)
 
 gl.drawArrays(gl.POINTS, 0, 1);
