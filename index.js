272ea8a9db23b12e333a0bde4ce682c36e1e3d2a
Christian Kitte <chkitte@web.de>
2022-10-20 16:36:25 +0200

    Von UNiform nach Attribute geswitched

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const gl = canvas.getContext("webgl2");
 
 const vertexShaderSource = `#version 300 es
     
-    uniform vec2 uPointPosition;
-    uniform float uPointSize;    
+    // uniform vec2 uPointPosition;
+    // uniform float uPointSize;    
+    
+    // Attribute
+    in vec2 uPointPosition;
+    in float uPointSize;
     
     void main()
     {
@@ -49,10 +53,35 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 
 gl.useProgram(program);
 
-const uPointSizeLocation = gl.getUniformLocation(program, "uPointSize");
-const uPointPositionLocation = gl.getUniformLocation(program, "uPointPosition");
+const bufferData = new Float32Array([
+    0.1, 0.1, 100,
+    0.5, -0.8, 32,
+    -0.9, 0.5, 50,
+])
 
-gl.uniform2f(uPointPositionLocation, 0.8, 0.2);
-gl.uniform1f(uPointSizeLocation, 25.0)
+// const uPointSizeLocation = gl.getUniformLocation(program, "uPointSize");
+// const uPointPositionLocation = gl.getUniformLocation(program, "uPointPosition");
+// gl.uniform2f(uPointPositionLocation, 0.8, 0.2);
+// gl.uniform1f(uPointSizeLocation, 25.0)
 
-gl.drawArrays(gl.POINTS, 0, 1);
+// Location ermitteln
+const uPointSizeLocation = gl.getAttribLocation(program, "uPointSize");
+const uPointPositionLocation = gl.getAttribLocation(program, "uPointPosition");
+
+// Location enablen
+gl.enableVertexAttribArray(uPointSizeLocation);
+gl.enableVertexAttribArray(uPointPositionLocation);
+
+// Buffer erzeugen und binden
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW);
+
+// Übergeben der Daten
+// Array Size: 3 Floats each 4 Bytes
+gl.vertexAttribPointer(uPointPositionLocation, 2, gl.FLOAT, false, 3 * 4, 0)
+gl.vertexAttribPointer(uPointSizeLocation, 1, gl.FLOAT, false, 3 * 4, 2 * 4)
+
+console.log(uPointSizeLocation, uPointPositionLocation);
+
+gl.drawArrays(gl.POINTS, 0, 3);
