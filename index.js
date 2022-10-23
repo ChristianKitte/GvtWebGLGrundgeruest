3055e10229b159710be56d573aa683ec8bfab258
Christian Kitte <chkitte@web.de>
2022-10-23 19:31:16 +0200

    Attribut Farbe implementiert, direkte Vergabe von Location

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const vertexShaderSource = `#version 300 es
     // uniform vec2 uPointPosition;
     // uniform float uPointSize;    
     
-    // Attribute
-    in vec2 uPointPosition;
-    in float uPointSize;
+    // in von JavaScript mit festgelegten Location/Zeiger)
+    layout(location = 1) in vec2 uPointPosition;
+    layout(location = 0) in float uPointSize;
+    layout(location = 2) in vec3 uColor;
+        
+    // out zu fragment shader
+    out vec3 vColor;
     
     void main()
     {
         // uPointPosition wird übernommen, der Rest statisch aufgefüllt
         gl_Position=vec4(uPointPosition, 0.0, 1.0);
         gl_PointSize=uPointSize;
+        
+        vColor=uColor;
         
        //gl_Position=vec4(0.5, 0.5, 0.0, 1.0);
        //gl_PointSize=150.0;
@@ -24,11 +30,16 @@ const vertexShaderSource = `#version 300 es
 const fragmentShaderSource = `#version 300 es
 
     precision mediump float;
+    
+    // in vom Vertex Shader
+    in vec3 vColor;
+
+    // out zu frame buffer und canvas
     out vec4 fragColor;
 
     void main()
     {
-       fragColor=vec4(1.0, 0.0, 0.0, 1.0);
+       fragColor=vec4(vColor,1.0);
     }
 `;
 
@@ -44,6 +55,21 @@ gl.shaderSource(fragmentShader, fragmentShaderSource);
 gl.compileShader(fragmentShader);
 gl.attachShader(program, fragmentShader);
 
+// Location/Pointer für die zu übergebenen Variablen selbst festlegen und enablen
+// Muss vor dem Linken erfolgen !
+uPointSizeLocation = 0;
+uPointPositionLocation = 1;
+uPointColorLocation = 2;
+
+// gl.bindAttribLocation(program, uPointSizeLocation, "uPointSize");
+// gl.bindAttribLocation(program, uPointPositionLocation, "uPointPosition");
+// gl.bindAttribLocation(program, uPointColorLocation, "uColor");
+
+// Location/Pointer enablen
+gl.enableVertexAttribArray(uPointSizeLocation);
+gl.enableVertexAttribArray(uPointPositionLocation);
+gl.enableVertexAttribArray(uPointColorLocation);
+
 gl.linkProgram(program);
 
 if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
@@ -53,10 +79,13 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 
 gl.useProgram(program);
 
+// Pos 1, 2         Position
+// Pos 2            Größe
+// Pos 3, 4, 5      RGB Wert
 const bufferData = new Float32Array([
-    0.1, 0.1, 100,
-    0.5, -0.8, 32,
-    -0.9, 0.5, 50,
+    0.1, 0.1, 100, 1, 0, 0,
+    0.5, -0.8, 32, 0, 1, 0,
+    -0.9, 0.5, 50, 0, 0, 1,
 ])
 
 // const uPointSizeLocation = gl.getUniformLocation(program, "uPointSize");
@@ -64,23 +93,30 @@ const bufferData = new Float32Array([
 // gl.uniform2f(uPointPositionLocation, 0.8, 0.2);
 // gl.uniform1f(uPointSizeLocation, 25.0)
 
-// Location ermitteln
-const uPointSizeLocation = gl.getAttribLocation(program, "uPointSize");
-const uPointPositionLocation = gl.getAttribLocation(program, "uPointPosition");
+// Location/Pointer für die zu übergebenen Variablen ermitteln und enablen
+// Kann nach dem Linken erfolgen !
 
-// Location enablen
-gl.enableVertexAttribArray(uPointSizeLocation);
-gl.enableVertexAttribArray(uPointPositionLocation);
+//const uPointSizeLocation = gl.getAttribLocation(program, "uPointSize");
+//const uPointPositionLocation = gl.getAttribLocation(program, "uPointPosition");
+//const uPointColorLocation = gl.getAttribLocation(program, "uColor");
 
-// Buffer erzeugen und binden
+//gl.enableVertexAttribArray(uPointSizeLocation);
+//gl.enableVertexAttribArray(uPointPositionLocation);
+//gl.enableVertexAttribArray(uPointColorLocation);
+
+// Ein neues Buffer erzeugen, binden und mit dem JavaScript Array verbinden
 const buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW);
 
 // Übergeben der Daten
-// Array Size: 3 Floats each 4 Bytes
-gl.vertexAttribPointer(uPointPositionLocation, 2, gl.FLOAT, false, 3 * 4, 0)
-gl.vertexAttribPointer(uPointSizeLocation, 1, gl.FLOAT, false, 3 * 4, 2 * 4)
+
+// Positionsdaten: 2 Byte groß ohne Offset
+gl.vertexAttribPointer(uPointPositionLocation, 2, gl.FLOAT, false, 6 * 4, 0)
+// Größendaten: 1 Byte groß ohne Offset 2 * 4 Byte (Positionsdaten)
+gl.vertexAttribPointer(uPointSizeLocation, 1, gl.FLOAT, false, 6 * 4, 2 * 4)
+// Farbdaten: 3 Byte groß ohne Offset 3 * 4 Byte (Positionsdaten + Größendaten)
+gl.vertexAttribPointer(uPointColorLocation, 3, gl.FLOAT, false, 6 * 4, 3 * 4)
 
 console.log(uPointSizeLocation, uPointPositionLocation);
 
